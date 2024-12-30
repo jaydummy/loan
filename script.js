21f3e77d1b42f34e534f2c6b4db9111c8c95798e
jaydummy <jaydummy07@gmail.com>
2024-12-30 14:29:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to the Loan Management System!");
     
     // Add event listeners to the "Learn More" buttons
-    const buttons = document.querySelectorAll('button');
+    const buttons = document.querySelectorAll('.learn-more-btn');
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             alert("More details about the loan will be available soon!");
